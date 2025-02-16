1d3d1b2c7c2117e717f35e224cc480f2d0d199be
DavidSaymmon <sawewfelipe@gmail.com>
2025-02-17 00:48:14 +0900

    implementacao da SellerDao, criacao da DaoFactory, teste na aplicacao

--- src/applicattion/Main.java
+++ src/applicattion/Main.java
@@ -2,14 +2,16 @@ package applicattion;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 	public static void main(String args[]) {
 		Department department = new Department(12, "Nome");
-		System.out.println(department);
 		Seller seller = new Seller(10, "Joao", "Joao@outlook.com", new Date(), 2500.0, new Department());
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
+	
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	public void insert(Seller seller);
+	public void update(Seller seller);
+	public void deleteById(Integer id);
+	public Seller findById(Integer id);
+	public List<Seller> findAll();
 }
